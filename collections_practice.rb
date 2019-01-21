ca86588787a4348073d9d5dac6a76af9a711a2ad
David Gilbert <dagilbert333@gmail.com>
2019-01-21 20:04:05 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -48,5 +48,5 @@ def sum_array(array)
 end
 
 def add_s(array)
- array.reject.with_index.collect{|element, index| element << "s"}
+ array.each_with_index.collect{|element, index| element << "s" - }
 end
